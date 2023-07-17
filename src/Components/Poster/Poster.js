c33a3558843ef82d1427fec116e356a31b4f075f
ajmalgithubm <ajmalm76774@gmail.com>
2023-07-17 19:07:59 +0530

    Responsive Property for image is Added in Poster component

--- src/Components/Poster/Poster.js
+++ src/Components/Poster/Poster.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './poster.css'
+import './Poster.css'
 function Poster() {
   return (
     <div className='p-2 bg-dark'>
@@ -9,45 +9,45 @@ function Poster() {
         </div> {/* Poster Heading is Given */}
         <div className='posterContent d-flex flex-row' style={{overflowY:'scroll'}}>
           {/* Multiple Poster are entering Here */}
-          <div className="poster">
+          <div className="poster p-2">
             <div className="posterImage">
-              <img src="https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326" style={{cursor:'pointer'}} alt="" />
+              <img src="https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326" style={{cursor:'pointer'}} className='img img-fluid' alt="" />
             </div>
             <div className="posterTitle pt-2 text-center">
               <p style={{ color: '#fff', fontFamily:'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif',fontWeight:'500',cursor:'pointer'}}>Money Heist</p>
             </div>
           </div>
 
-          <div className="poster">
+          <div className="poster p-2">
             <div className="posterImage">
-              <img src="https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326" style={{ cursor: 'pointer' }} alt="" />
+              <img src="https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326" style={{ cursor: 'pointer' }} alt="" className='img img-fluid' />
             </div>
             <div className="posterTitle pt-2 text-center">
               <p style={{ color: '#fff', fontFamily: 'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif', fontWeight: '500', cursor: 'pointer' }}>Money Heist</p>
             </div>
           </div>
 
-          <div className="poster">
+          <div className="poster p-2">
             <div className="posterImage">
-              <img src="https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326" style={{ cursor: 'pointer' }} alt="" />
+              <img src="https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326" style={{ cursor: 'pointer' }} alt="" className='img img-fluid' />
             </div>
             <div className="posterTitle pt-2 text-center">
               <p style={{ color: '#fff', fontFamily: 'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif', fontWeight: '500', cursor: 'pointer' }}>Money Heist</p>
             </div>
           </div>
 
-          <div className="poster">
+          <div className="poster p-2">
             <div className="posterImage">
-              <img src="https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326" style={{ cursor: 'pointer' }} alt="" />
+              <img src="https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326" style={{ cursor: 'pointer' }} alt="" className='img img-fluid' />
             </div>
             <div className="posterTitle pt-2 text-center">
               <p style={{ color: '#fff', fontFamily: 'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif', fontWeight: '500', cursor: 'pointer' }}>Money Heist</p>
             </div>
           </div>
 
-          <div className="poster">
+          <div className="poster p-2">
             <div className="posterImage">
-              <img src="https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326" style={{ cursor: 'pointer' }} alt="" />
+              <img src="https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326" style={{ cursor: 'pointer' }} alt="" className='img img-fluid' />
             </div>
             <div className="posterTitle pt-2 text-center">
               <p style={{ color: '#fff', fontFamily: 'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif', fontWeight: '500', cursor: 'pointer' }}>Money Heist</p>
