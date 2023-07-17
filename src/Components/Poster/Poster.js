eef943da6a09a4f5bc3a46f24f96aacfa8239204
ajmalgithubm <ajmalm76774@gmail.com>
2023-07-17 19:14:11 +0530

    Resposivness For text is Added

--- src/Components/Poster/Poster.js
+++ src/Components/Poster/Poster.js
@@ -4,8 +4,8 @@ function Poster() {
   return (
     <div className='p-2 bg-dark'>
       <div className='entirePosterContent d-flex flex-column'>
-        <div className='posterHeading'>
-          <p style={{ color: '#fff', fontFamily: 'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif', fontWeight: '600', cursor: 'pointer' }}>Popular On Netflix</p>
+        <div className='posterHeading p-2'>
+          <p style={{ color: '#fff', fontFamily: 'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif', fontWeight: '600', cursor: 'pointer' ,fontSize:'1.5vw'}}>Popular On Netflix</p>
         </div> {/* Poster Heading is Given */}
         <div className='posterContent d-flex flex-row' style={{overflowY:'scroll'}}>
           {/* Multiple Poster are entering Here */}
@@ -14,7 +14,7 @@ function Poster() {
               <img src="https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326" style={{cursor:'pointer'}} className='img img-fluid' alt="" />
             </div>
             <div className="posterTitle pt-2 text-center">
-              <p style={{ color: '#fff', fontFamily:'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif',fontWeight:'500',cursor:'pointer'}}>Money Heist</p>
+              <p style={{ color: '#fff', fontFamily:'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif',fontWeight:'500',cursor:'pointer',fontSize:'1.2vw'}}>Money Heist</p>
             </div>
           </div>
 
@@ -23,7 +23,7 @@ function Poster() {
               <img src="https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326" style={{ cursor: 'pointer' }} alt="" className='img img-fluid' />
             </div>
             <div className="posterTitle pt-2 text-center">
-              <p style={{ color: '#fff', fontFamily: 'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif', fontWeight: '500', cursor: 'pointer' }}>Money Heist</p>
+              <p style={{ color: '#fff', fontFamily: 'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif', fontWeight: '500', cursor: 'pointer', fontSize: '1.2vw' }}>Money Heist</p>
             </div>
           </div>
 
@@ -32,7 +32,7 @@ function Poster() {
               <img src="https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326" style={{ cursor: 'pointer' }} alt="" className='img img-fluid' />
             </div>
             <div className="posterTitle pt-2 text-center">
-              <p style={{ color: '#fff', fontFamily: 'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif', fontWeight: '500', cursor: 'pointer' }}>Money Heist</p>
+              <p style={{ color: '#fff', fontFamily: 'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif', fontWeight: '500', cursor: 'pointer', fontSize: '1.2vw' }}>Money Heist</p>
             </div>
           </div>
 
@@ -41,7 +41,7 @@ function Poster() {
               <img src="https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326" style={{ cursor: 'pointer' }} alt="" className='img img-fluid' />
             </div>
             <div className="posterTitle pt-2 text-center">
-              <p style={{ color: '#fff', fontFamily: 'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif', fontWeight: '500', cursor: 'pointer' }}>Money Heist</p>
+              <p style={{ color: '#fff', fontFamily: 'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif', fontWeight: '500', cursor: 'pointer', fontSize: '1.2vw' }}>Money Heist</p>
             </div>
           </div>
 
@@ -50,7 +50,7 @@ function Poster() {
               <img src="https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326" style={{ cursor: 'pointer' }} alt="" className='img img-fluid' />
             </div>
             <div className="posterTitle pt-2 text-center">
-              <p style={{ color: '#fff', fontFamily: 'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif', fontWeight: '500', cursor: 'pointer' }}>Money Heist</p>
+              <p style={{ color: '#fff', fontFamily: 'Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif', fontWeight: '500', cursor: 'pointer', fontSize: '1.2vw' }}>Money Heist</p>
             </div>
           </div>
         </div>
